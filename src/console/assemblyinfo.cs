72b60429c83f1291b959d345790b9cd1423ad62b
var1ap <varushin-a-p@mail.ru>
2016-12-01 19:46:29 +0400

    Initial migration to VS 2017 projects

--- src/console/assemblyinfo.cs
+++ src/console/assemblyinfo.cs
@@ -4,15 +4,15 @@ using System.Security.Permissions;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-[assembly: System.Reflection.AssemblyProduct("Python for .NET")]
-[assembly: System.Reflection.AssemblyVersion("2.4.2.7")]
-[assembly: AssemblyTitleAttribute("Python Console")]
+//[assembly: System.Reflection.AssemblyProduct("Python for .NET")]
+//[assembly: System.Reflection.AssemblyVersion("2.4.2.7")]
+//[assembly: AssemblyTitleAttribute("Python Console")]
 [assembly: AssemblyDefaultAliasAttribute("python.exe")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: PermissionSetAttribute(SecurityAction.RequestMinimum,
     Name = "FullTrust")]
-[assembly: AssemblyDescriptionAttribute("")]
+//[assembly: AssemblyDescriptionAttribute("")]
 [assembly: AssemblyCopyrightAttribute("Zope Public License, Version 2.0 (ZPL)")]
-[assembly: AssemblyFileVersionAttribute("2.0.0.4")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
+//[assembly: AssemblyFileVersionAttribute("2.0.0.4")]
+//[assembly: NeutralResourcesLanguageAttribute("en")]
--- src/runtime/assemblyinfo.cs
+++ src/runtime/assemblyinfo.cs
@@ -3,14 +3,14 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-[assembly: AssemblyProduct("Python for .NET")]
-[assembly: AssemblyVersion("4.0.0.1")]
-[assembly: AssemblyDefaultAliasAttribute("Python.Runtime.dll")]
+//[assembly: AssemblyProduct("Python for .NET")]
+//[assembly: AssemblyVersion("4.0.0.1")]
+//[assembly: AssemblyDefaultAliasAttribute("Python.Runtime.dll")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: AssemblyCopyrightAttribute("Zope Public License, Version 2.0 (ZPL)")]
-[assembly: AssemblyFileVersionAttribute("2.0.0.2")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
+//[assembly: AssemblyCopyrightAttribute("Zope Public License, Version 2.0 (ZPL)")]
+//[assembly: AssemblyFileVersionAttribute("2.0.0.2")]
+//[assembly: NeutralResourcesLanguageAttribute("en")]
 
 #if (PYTHON23)
 [assembly: AssemblyTitleAttribute("Python.Runtime for Python 2.3")]
